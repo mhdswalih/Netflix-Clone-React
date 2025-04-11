8b39477e59f87e4b6b157d902652dd73d4ed3100
mhdswalih <ctmswalihmc@gmail.com>
2025-04-11 22:26:23 +0530

    addedPlay icon

--- src/components/Pages/Home/Home.jsx
+++ src/components/Pages/Home/Home.jsx
@@ -3,7 +3,7 @@ import './Home.css'
 import Navbar from '../../Navbar/Navbar'
 import hero_banner from '../../../assets/hero_banner.jpg'
 import hero_title from '../../../assets/hero_title.png'
-import play_icon from '../../../assets/Play_icon.png'
+import Play_icon from '../../../assets/Play_icon.png'
 import info_icon from '../../../assets/info_icon.png'
 import TitleCard from '../../TitleCards/TitleCard'
 import Footer from '../../Footer/Footer'
@@ -19,7 +19,7 @@ const Home = () => {
         <img src={hero_title} alt="" className='cation-img'/>
         <p>Discover his ties to a secret ancient order. a young man living in modern Istanbul embarks on a quest to save the city from an immortal enemy</p>
        <div className="hero-btns">
-        <button className='btn'><img src={play_icon} alt="" />Play</button>
+        <button className='btn'><img src={Play_icon} alt="" />Play</button>
         <button className='btn dark-btn'><img src={info_icon} alt="" />More Info</button>
        </div>
        <TitleCard />
