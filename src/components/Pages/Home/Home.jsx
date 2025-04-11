35ae3b4f9e2030f64d7b4a08d69968a72f7bacc3
mhdswalih <ctmswalihmc@gmail.com>
2025-04-11 22:23:53 +0530

    thirdcommit

--- src/components/Pages/Home/Home.jsx
+++ src/components/Pages/Home/Home.jsx
@@ -17,7 +17,7 @@ const Home = () => {
         <img src={hero_banner} alt="" className='banner-img' />
         <div className="hero-caption"> 
         <img src={hero_title} alt="" className='cation-img'/>
-        <p>Discover his ties to a secret ancient order. a young man living in modern Istanbul embraks on a quest to save the city from an immortal enemy</p>
+        <p>Discover his ties to a secret ancient order. a young man living in modern Istanbul embarks on a quest to save the city from an immortal enemy</p>
        <div className="hero-btns">
         <button className='btn'><img src={play_icon} alt="" />Play</button>
         <button className='btn dark-btn'><img src={info_icon} alt="" />More Info</button>
